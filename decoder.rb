8004d7ddc4b83e55eeebcf68c03b83735d4fd3bf
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-10-12 21:59:30 +0300

    Tweaked code style for better readability

--- decoder.rb
+++ decoder.rb
@@ -1,64 +1,25 @@
 MORSE_CODE = {
-  'A' => '.-',
-  'B' => '-...',
-  'C' => '-.-.',
-  'D' => '-..',
-  'E' => '.',
-  'F' => '..-.',
-  'G' => '--.',
-  'H' => '....',
-  'I' => '..',
-  'J' => '.---',
-  'K' => '-.-',
-  'L' => '.-..',
-  'M' => '--',
-  'N' => '-.',
-  'O' => '---',
-  'P' => '.--.',
-  'Q' => '--.-',
-  'R' => '.-.',
-  'S' => '...',
-  'T' => '-',
-  'U' => '..-',
-  'V' => '...-',
-  'W' => '.--',
-  'X' => '-..-',
-  'Y' => '-.--',
-  'Z' => '--..',
-  '0' => '-----',
-  '1' => '.----',
-  '2' => '..---',
-  '3' => '...--',
-  '4' => '....-',
-  '5' => '.....',
-  '6' => '-....',
-  '7' => '--...',
-  '8' => '---..',
-  '9' => '----.'
+  'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.', 'G' => '--.', 'H' => '....',
+  'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..', 'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.',
+  'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-', 'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-',
+  'Y' => '-.--', 'Z' => '--..', '0' => '-----', '1' => '.----', '2' => '..---', '3' => '...--', '4' => '....-',
+  '5' => '.....', '6' => '-....', '7' => '--...', '8' => '---..', '9' => '----.'
 }.freeze
 
 def decode_character(character)
-  MORSE_CODE.each do |key, value|
-    return key if value == character
-  end
+  MORSE_CODE.each { |key, value| return key if value == character }
 end
 
 def decode_word(word)
   result = ''
-  characters = word.split
-  characters.each do |character|
-    result += decode_character(character)
-  end
+  word.split.each { |character| result += decode_character(character) }
   result
 end
 
 def decode_message(message)
   result = []
-  words = message.strip.upcase.split('   ')
-  words.each do |word|
-    result.append(decode_word(word))
-  end
+  message.strip.split('   ').each { |word| result.append(decode_word(word)) }
   result.join(' ')
 end
 
-puts decode_message '      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+puts decode_message('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
